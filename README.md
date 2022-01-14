3223b9181eb48b1eff2b291be3b5d70279272a41
Renan Lira <renanlira@Renans-MacBook-Air.local>
2022-01-14 20:28:02 -0300

    :tada: the beginning

---
+++ README.md
@@ -0,0 +1,26 @@
+# Bash-it Themes
+
+## Installation
+
+First you need to install bash-it. To see how, visit:
+
+````https://github.com/Bash-it/bash-it````
+
+Copy the directory themes to bash-it theme directory:
+
+````.bash-it/themes/````
+
+Then you can change the theme name on your .bash_rc or .bash_profile file, usually on line:
+
+````export BASH_IT_THEME='theme_name'````
+
+----
+
+#### Magiccube
+A theme based on Powerline. For more information, see:
+
+````https://github.com/Bash-it/bash-it/tree/master/themes/powerline````
+
+#### Sushu
+A clean theme useful in light and dark terminal
+
---
+++ sushu/sushu.theme.bash
@@ -0,0 +1,36 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034 # Expected behavior for themes.
+
+SCM_THEME_PROMPT_DIRTY=" ${red?}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green?}✓"
+SCM_THEME_PROMPT_PREFIX=" ${green?}|"
+SCM_THEME_PROMPT_SUFFIX="${green?}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red?}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green?}✓"
+GIT_THEME_PROMPT_PREFIX=" ${cyan?}| "
+GIT_THEME_PROMPT_SUFFIX="${cyan?} |"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+: "${THEME_CLOCK_CHAR_COLOR:=${red?}}"
+: "${THEME_CLOCK_FORMAT:="%d %b %y %H:%M"}"
+
+function sushu_clock() {
+	printf '%s' "$(clock_prompt) "
+}
+
+function sushu_prompt_command() {
+	PS1="${cyan?}╭─"
+    PS1+="\n$(battery_char) \[\e[15m$(sushu_clock)${reset_color?}~ "
+	PS1+="${normal}\[\e[1m\e[38;5;44m\]\u ${reset_color}"
+	PS1+="${normal}\[\e[38;5;104m\]\w\n${reset_color?}"
+	PS1+="${cyan}╰─ \$ ${normal}"
+    PS1+="${bold_cyan?}$(scm_prompt_char_info) "
+	PS1+="${cyan?}→${reset_color?}${BOLD} "
+}
+
+
+safe_append_prompt_command sushu_prompt_command
+
