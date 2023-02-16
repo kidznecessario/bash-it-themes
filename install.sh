670eaa3a4e97404b273671392f34d37165f1900a
Renan Lira <renan.liraj@gmail.com>
2023-02-16 09:41:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo -e "Creating a symbolic link to bash-it themes directory"
-ln -s $HOME/github/therenanlira/bash-it-themes/magiccube $HOME/.bash_it/themes/
-ln -s $HOME/github/therenanlira/bash-it-themes/sushu $HOME/.bash_it/themes/
-ln -s $HOME/github/therenanlira/bash-it-themes/new-sushu $HOME/.bash_it/themes/
+ln -s $HOME/.bash-it-themes/magiccube $HOME/.bash_it/themes/
+ln -s $HOME/.bash-it-themes/sushu $HOME/.bash_it/themes/
+ln -s $HOME/.bash-it-themes/new-sushu $HOME/.bash_it/themes/
