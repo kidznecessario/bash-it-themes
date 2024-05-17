25d80047717c6e77f1dbefd1de01b05db1b66afd
Renan <vison-banners-0l@icloud.com>
2024-05-17 17:23:05 -0300

    chore: Create symbolic links for bash-it aliases in install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ echo -e "Creating a symbolic link to bash-it themes directory"
 sudo ln -s $HOME/.bash-it-themes/magiccube $HOME/.bash_it/themes/
 sudo ln -s $HOME/.bash-it-themes/sushu $HOME/.bash_it/themes/
 sudo ln -s $HOME/.bash-it-themes/new-sushu $HOME/.bash_it/themes/
+
+echo -e "Creating a symbolic link to bash-it aliases directory"
+sudo ln -s $HOME/.bash-it-aliases/magiccube.aliases.bash $HOME/.bash_it/aliases/
+sudo ln -s $HOME/.bash-it-aliases/sushu.aliases.bash $HOME/.bash_it/aliases/
+sudo ln -s $HOME/.bash-it-aliases/new-sushu.aliases.bash $HOME/.bash_it/aliases/
